c2f642ce255fd815fb17581b1e311b994f7391e8
maaly <maalymostafa@gmail.com>
2026-06-26 16:44:49 +0300

    Add style preview options

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 const dashboardController = require("../controllers/dashboardController");
-const { requireApiAuth } = require("../middleware/authMiddleware");
+const { requireApiAuth, requireAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.get("/style-preview", requireAuth, (req, res) => {
+  return res.sendFile(path.join(__dirname, "..", "views", "style-preview.html"));
+});
 router.get("/api/dashboard", requireApiAuth, dashboardController.getDashboard);
 
 module.exports = router;
